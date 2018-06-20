78f2f07788c6a06e0edc23c3ec990aeb651ca6ee
dalecameron <fortiscontego@gmail.com>
2018-06-20 09:47:53 -0500

    removed glider-js

--- demo/src/app/instance-manager/instance.service.ts
+++
@@ -1,9 +0,0 @@
-//used to hand base urls and authentication with an instance
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class InstanceService {
-
-  constructor() { }
-
-}
